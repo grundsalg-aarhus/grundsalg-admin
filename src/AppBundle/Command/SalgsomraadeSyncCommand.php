390cbc7093e211c6574f3dfab8dd8b67e2a2ff69
turegjorup <ture@ignatz.dk>
2017-02-20 12:26:30 +0100

    Added command to sync all “salgsomraader”

---
+++ src/AppBundle/Command/SalgsomraadeSyncCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\Lokalplan;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Sync salgsomraader command
+ */
+class SalgsomraadeSyncCommand extends ContainerAwareCommand
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure()
+  {
+    $this
+      ->setName('app:salgsomraader:sync')
+      ->setDescription('Sync alle salgsomraader til Drupal');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $this->output = $output;
+
+    $comService = $this->getContainer()->get('grundsalg.communication');
+
+    $em = $this->getContainer()->get('doctrine')->getManager();
+    $salgsomraader = $em->getRepository('AppBundle:Salgsomraade')->findAll();
+    $count = 0;
+
+      foreach ($salgsomraader as $salgsomraade) {
+        $comService->saveSalgsomraade($salgsomraade);
+        $output->writeln('Sync '.$salgsomraade->getId().' '.$salgsomraade->getTitel());
+        $count++;
+      }
+
+    $output->writeln('Synced '.$count.' salgsomraader');
+
+  }
+
+}
