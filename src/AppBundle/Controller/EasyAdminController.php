5e196e23bdb4b9b6137cf68f3bedfb17a8bec424
turegjorup <ture@ignatz.dk>
2017-03-17 14:17:17 +0100

    GRUND-266: Fixed bug in searchQueryBuilder where DQL filter for list view not used

--- src/AppBundle/Controller/EasyAdminController.php
+++ src/AppBundle/Controller/EasyAdminController.php
@@ -13,19 +13,18 @@ class EasyAdminController extends BaseAdminController {
    * Modified to:
    * - Enable sorting on relations of 1+N depth
    *
-   * @param string      $entityClass
-   * @param string      $sortDirection
+   * @param string $entityClass
+   * @param string $sortDirection
    * @param string|null $sortField
    * @param string|null $dqlFilter
    *
    * @return QueryBuilder The Query Builder instance
    */
-  protected function createListQueryBuilder($entityClass, $sortDirection, $sortField = null, $dqlFilter = null)
-  {
+  protected function createListQueryBuilder($entityClass, $sortDirection, $sortField = NULL, $dqlFilter = NULL) {
     $queryBuilder = parent::createListQueryBuilder($entityClass, $sortDirection, $sortField, $dqlFilter);
 
     // We only care about the sortfield
-    if(!empty($sortField)) {
+    if (!empty($sortField)) {
 
       // Split the sortfield by '.' to get relations
       $joinParts = explode('.', $sortField);
@@ -98,25 +97,27 @@ class EasyAdminController extends BaseAdminController {
     // Get the deafult query based on the list fields (- but missing relations)
     $queryBuilder = parent::createSearchQueryBuilder($entityClass, $searchQuery, $searchableFields, $sortField, $sortDirection, $dqlFilter);
 
-    // To search in fields in related entities we need to build our own joins
+    // To search in fields in related entities we need to build our own joins and where
     $queryBuilder->resetDQLPart('join');
+    $queryBuilder->resetDQLPart('where');
 
     // Maintain a list of added joins to avoid adding duplicates
     $joins = array();
 
     // Loop through list fields to add fields in relations
+    $queryParameters = array();
     foreach ($this->entity['list']['fields'] as $name => $metadata) {
 
       // Split the field name by '.' to get relations
       $joinParts = explode('.', $name);
       $countParts = count($joinParts);
 
+      // 'Entity' is name for the base entity used in the parent builder
+      $entityDqlName = 'entity';
+      $fieldDqlName = $name;
+
       // Only process relations
       if ($countParts > 1) {
-
-        // 'Entity' is name for the base entity used in the parent builder
-        $entityDqlName = 'entity';
-        $fieldDqlName = $name;
 
         // We don't know the depth of the relations, so loop through the parts and add joins for all
         // The last element is the fieldname, so stop at count - 1
@@ -141,53 +142,60 @@ class EasyAdminController extends BaseAdminController {
           $entityDqlName = $joinParts[$i];
           $fieldDqlName = $joinParts[$i + 1];
         }
-
-        // Add 'where' clauses based on field type - largely identical to logic in parent logic
-        $isNumericField = in_array($metadata['dataType'], array(
-          'integer',
-          'number',
-          'smallint',
-          'bigint',
-          'decimal',
-          'float'
-        ));
-        $isTextField = in_array($metadata['dataType'], array(
-          'string',
-          'text',
-          'guid'
-        ));
-        $isGuidField = 'guid' === $metadata['dataType'];
-
-        if ($isNumericField && is_numeric($searchQuery)) {
-          $queryBuilder->orWhere(sprintf('%s.%s = :exact_query', $entityDqlName, $fieldDqlName));
-          // adding '0' turns the string into a numeric value
-          $queryBuilder->setParameter('exact_query', 0 + $searchQuery);
-        }
-        elseif ($isGuidField) {
-          // some databases don't support LOWER() on UUID fields
-          $queryBuilder->orWhere(sprintf('%s.%s IN (:words_query)', $entityDqlName, $fieldDqlName));
-          $queryBuilder->setParameter('words_query', explode(' ', $searchQuery));
-        }
-        elseif ($isTextField) {
-          $searchQuery = mb_strtolower($searchQuery);
-
-          $queryBuilder->orWhere(sprintf('LOWER(%s.%s) LIKE :fuzzy_query', $entityDqlName, $fieldDqlName));
-          $queryBuilder->setParameter('fuzzy_query', '%' . $searchQuery . '%');
-
-          $queryBuilder->orWhere(sprintf('LOWER(%s.%s) IN (:words_query)', $entityDqlName, $fieldDqlName));
-          $queryBuilder->setParameter('words_query', explode(' ', $searchQuery));
-        }
-
-        // Add the field to list of search fields
-        $this->entity['search']['fields'][$name] = $metadata;
-
-        // The parent queryBuilder only supports orderBy on direct relations so we reset and rebuild if it's deeper
-        if ($countParts > 2 && $name === $sortField) {
-          $queryBuilder->resetDQLPart('orderBy');
-          $queryBuilder->orderBy($entityDqlName . '.' . $fieldDqlName, $sortDirection ?: 'DESC');
-        }
       }
 
+      // Add 'where' clauses based on field type - largely identical to logic in parent logic
+      $isNumericField = in_array($metadata['dataType'], array(
+        'integer',
+        'number',
+        'smallint',
+        'bigint',
+        'decimal',
+        'float'
+      ));
+      $isTextField = in_array($metadata['dataType'], array(
+        'string',
+        'text',
+        'guid'
+      ));
+      $isGuidField = 'guid' === $metadata['dataType'];
+
+      if ($isNumericField && is_numeric($searchQuery)) {
+        $queryBuilder->orWhere(sprintf('%s.%s = :exact_query', $entityDqlName, $fieldDqlName));
+        // adding '0' turns the string into a numeric value
+        $queryBuilder->setParameter('exact_query', 0 + $searchQuery);
+      }
+      elseif ($isGuidField) {
+        // some databases don't support LOWER() on UUID fields
+        $queryBuilder->orWhere(sprintf('%s.%s IN (:words_query)', $entityDqlName, $fieldDqlName));
+        $queryBuilder->setParameter('words_query', explode(' ', $searchQuery));
+      }
+      elseif ($isTextField) {
+        $searchQuery = mb_strtolower($searchQuery);
+
+        $queryBuilder->orWhere(sprintf('LOWER(%s.%s) LIKE :fuzzy_query', $entityDqlName, $fieldDqlName));
+        $queryBuilder->setParameter('fuzzy_query', '%' . $searchQuery . '%');
+
+        $queryBuilder->orWhere(sprintf('LOWER(%s.%s) IN (:words_query)', $entityDqlName, $fieldDqlName));
+        $queryBuilder->setParameter('words_query', explode(' ', $searchQuery));
+      }
+
+      // Add the field to list of search fields
+      $this->entity['search']['fields'][$name] = $metadata;
+
+      // The parent queryBuilder only supports orderBy on direct relations so we reset and rebuild if it's deeper
+      if ($countParts > 2 && $name === $sortField) {
+        $queryBuilder->resetDQLPart('orderBy');
+        $queryBuilder->orderBy($entityDqlName . '.' . $fieldDqlName, $sortDirection ?: 'DESC');
+      }
+    }
+
+    if (0 !== count($queryParameters)) {
+      $queryBuilder->setParameters($queryParameters);
+    }
+
+    if (!empty($dqlFilter)) {
+      $queryBuilder->andWhere($dqlFilter);
     }
 
     return $queryBuilder;
