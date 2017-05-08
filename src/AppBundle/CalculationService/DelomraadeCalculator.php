ddebf014595ec7009e767bbe81f55082d18922a1
turegjorup <ture@ignatz.dk>
2017-05-08 14:53:18 +0200

    GRUND-341: Implemented OpkoebCalculator

--- src/AppBundle/CalculationService/DelomraadeCalculator.php
+++ src/AppBundle/CalculationService/DelomraadeCalculator.php
@@ -21,22 +21,32 @@ class DelomraadeCalculator implements EventSubscriber {
   }
 
   public function prePersist(LifecycleEventArgs $args) {
-    $lokalplan = $args->getEntity();
+    $delomraade = $args->getEntity();
 
     // only act on some "Delomraade" entity
-    if (!$lokalplan instanceof Delomraade) {
+    if (!$delomraade instanceof Delomraade) {
       return;
     }
 
-    $this->calculateKpl1($lokalplan);
-    $this->calculateO1($lokalplan);
+    $this->calculateKpl1($delomraade);
+    $this->calculateO1($delomraade);
 
   }
 
+  /**
+   * Set 'kpl1' as 'Lokalsamfund' number
+   *
+   * @param \AppBundle\Entity\Delomraade $delomraade
+   */
   private function calculateKpl1(Delomraade $delomraade) {
     $delomraade->setKpl1($delomraade->getLokalplan()->getLokalsamfund()->getNumber());
   }
 
+  /**
+   * Set 'O1' value to 'Lokalplan' number if it's empty
+   *
+   * @param \AppBundle\Entity\Delomraade $delomraade
+   */
   private function calculateO1(Delomraade $delomraade) {
     if(!$delomraade->getO1()) {
       $delomraade->setO1($delomraade->getLokalplan()->getNr());
