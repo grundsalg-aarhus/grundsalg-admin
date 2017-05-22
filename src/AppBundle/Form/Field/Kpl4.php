92b7468ec0f617459a9820a02aa370cd128b8688
turegjorup <ture@ignatz.dk>
2017-05-22 11:24:53 +0200

    GRUND-329: Refactored custom form types

--- src/AppBundle/Form/Field/Kpl4.php
+++ src/AppBundle/Form/Field/Kpl4.php
@@ -3,17 +3,15 @@
 namespace AppBundle\Form\Field;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use AppBundle\DBAL\Types\Kpl4 as Kpl4Type;
 
 class Kpl4 extends AbstractType
 {
   public function configureOptions(OptionsResolver $resolver)
   {
     $resolver->setDefaults(array(
-      'choices' => Kpl4Type::getChoices()
+      'choices' => \AppBundle\DBAL\Types\Kpl4Type::getChoices()
       )
     );
   }
