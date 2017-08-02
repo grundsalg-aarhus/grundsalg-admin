edd1d99d5ab57249295fae170a6fe1d3b64c1020
Ture Gjørup <ture@ignatz.dk>
2017-08-02 11:43:25 +0200

    GRUND-418: Fixed bug where virtual columns appeared sortable

--- src/AppBundle/Configuration/EasyAdminConfigManager.php
+++ src/AppBundle/Configuration/EasyAdminConfigManager.php
@@ -57,6 +57,21 @@ class EasyAdminConfigManager extends ConfigManager {
       }
     }
 
+    // We have modified createListQueryBuilder to work with associations,
+    // so we need to set 'sortable' => true on those fields for the list templates to pick it up
+    if(is_array($config) && array_key_exists('entities', $config)) {
+      foreach ($config['entities'] as &$entity) {
+        foreach ($entity['list']['fields'] as $fieldName => &$fieldConfig) {
+          $fieldParts = explode('.', $fieldName);
+          $countParts = count($fieldParts);
+
+          if($countParts > 1) {
+            $fieldConfig['sortable'] = true;
+          }
+        }
+      }
+    }
+
     $this->cache[$cacheKey] = $config;
 
     return $config;
