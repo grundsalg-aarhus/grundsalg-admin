9be84eb1341249797f081866540b0c1506189c4a
Ture Gjørup <ture@ignatz.dk>
2017-08-10 12:44:32 +0200

    GRUND-422: Fixed matrik2 not nullable on 'Opkoeb'

--- app/DoctrineMigrations/Version20170508125229.php
+++ app/DoctrineMigrations/Version20170508125229.php
@@ -18,7 +18,7 @@ class Version20170508125229 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Opkoeb CHANGE matrik1 matrik1 VARCHAR(50) NOT NULL, CHANGE matrik2 matrik2 VARCHAR(50) NOT NULL, CHANGE ejerlav ejerlav VARCHAR(60) NOT NULL, CHANGE m2 m2 INT NOT NULL, CHANGE opkoebDato opkoebDato DATE NOT NULL, CHANGE pris pris DOUBLE PRECISION NOT NULL');
+        $this->addSql('ALTER TABLE Opkoeb CHANGE matrik1 matrik1 VARCHAR(50) NOT NULL, CHANGE ejerlav ejerlav VARCHAR(60) NOT NULL, CHANGE m2 m2 INT NOT NULL, CHANGE opkoebDato opkoebDato DATE NOT NULL, CHANGE pris pris DOUBLE PRECISION NOT NULL');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20170508125229 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE Opkoeb CHANGE matrik1 matrik1 VARCHAR(50) DEFAULT NULL COLLATE utf8_general_ci, CHANGE matrik2 matrik2 VARCHAR(50) DEFAULT NULL COLLATE utf8_general_ci, CHANGE ejerlav ejerlav VARCHAR(60) DEFAULT NULL COLLATE utf8_general_ci, CHANGE m2 m2 INT DEFAULT NULL, CHANGE opkoebDato opkoebDato DATE DEFAULT NULL, CHANGE pris pris DOUBLE PRECISION DEFAULT NULL');
+        $this->addSql('ALTER TABLE Opkoeb CHANGE matrik1 matrik1 VARCHAR(50) DEFAULT NULL COLLATE utf8_general_ci, CHANGE ejerlav ejerlav VARCHAR(60) DEFAULT NULL COLLATE utf8_general_ci, CHANGE m2 m2 INT DEFAULT NULL, CHANGE opkoebDato opkoebDato DATE DEFAULT NULL, CHANGE pris pris DOUBLE PRECISION DEFAULT NULL');
     }
 }
