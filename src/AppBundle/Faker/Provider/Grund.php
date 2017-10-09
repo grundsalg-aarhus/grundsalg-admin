9ac09ae80dddc67ab8c3f9f4906a26c4164af671
Ture Gjørup <ture@ignatz.dk>
2017-10-09 20:20:40 +0200

    GRUND-438: Fixed Behat/Faker context after schema changed

--- src/AppBundle/Faker/Provider/Grund.php
+++ src/AppBundle/Faker/Provider/Grund.php
@@ -12,13 +12,60 @@ use AppBundle\DBAL\Types\GrundType;
 use AppBundle\DBAL\Types\SalgsType;
 use Faker\Provider\Base;
 
-class Grund extends Base {
-  public function type()
-  {
-    return GrundType::PARCELHUS;
-  }
+class Grund extends Base
+{
+    public function type()
+    {
+        return GrundType::PARCELHUS;
+    }
 
-  public function salgsType() {
-    return SalgsType::FASTPRIS;
-  }
+    public function salgsType()
+    {
+        return SalgsType::FASTPRIS;
+    }
+
+    public function maxetagem2()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 9000);
+    }
+
+    public function areal()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 500000);
+    }
+
+    public function arealvej()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 4000);
+    }
+
+    public function arealkotelet()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 11000);
+    }
+
+    public function bruttoareal()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 11000);
+    }
+
+    public function prism2()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 813000);
+    }
+
+    public function prisfoerkorrektion()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 9706000);
+    }
+
+    public function korr1()
+    {
+        return $this->generator->randomFloat($min = -2003408, $max = 1679000);
+    }
+
+    public function pris()
+    {
+        return $this->generator->randomFloat($min = 0, $max = 135525000);
+    }
 }
