505eaec7c932c9309e54af36867efabcfb0e2f19
Ture Gjørup <ture@ignatz.dk>
2017-11-15 15:23:39 +0100

    GRUND-443: Changed order of 'Loggable' migration

--- app/DoctrineMigrations/Version00000000000000.php
+++ app/DoctrineMigrations/Version00000000000000.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20171115133145 extends AbstractMigration
+class Version00000000000000 extends AbstractMigration
 {
     /**
      * @param Schema $schema
